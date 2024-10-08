e8d0e192c0dfbc2630b94617b4f0e94ff1bf9947
trngkhang <nguyenkhangdev@gmail.com>
2024-10-08 21:12:22 +0700

    cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import "dotenv/config";
 import okRoutes from "./routes/ok.routes.js";
+import process from "process";
+
 const app = express();
 const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 app.use("/static", express.static(path.join(__dirname, "public")));
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL.toString(),
+};
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
